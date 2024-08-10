7f04db6cb0cccabd56195f2eb46ccbb2378f3c20
SuryaTejaswini <21131a05r4@gvpce.ac.in>
2024-08-10 10:29:42 +0530

    CryptoCanvas files

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>CopyRight @ 2024, CryptoCanvas - All Right Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
